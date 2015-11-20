f80f7150e70c881eae4e264bed9eb99f78ff7b11
sebroy00 <sroy065@uottawa.ca>
2015-11-20 14:47:20 -0500

    Reparer bug du compte d'animaux qui ont une connection (paired)

--- VisualStudio_files/CanadianSevenDragons/table.cpp
+++ VisualStudio_files/CanadianSevenDragons/table.cpp
@@ -7,38 +7,38 @@ int isConnected(std::shared_ptr<AnimalCard> around[4], std::shared_ptr<AnimalCar
 	if (around[0] != 0) {
 		paired[0] |= around[0]->getAnimalAt(1) == card->getAnimalAt(0);
 		paired[2] |= around[0]->getAnimalAt(3) == card->getAnimalAt(2) ? 1 : 0;
+	}
 		if (card->getAnimalAt(0) == card->getAnimalAt(2)) {
 			paired[0] |= paired[2];
 			paired[2] = 0; //on garde le compte pour seulement une carte
 		}
-	}
 	/*Top*/
 	if (around[1] != 0) {
 		paired[0] |= around[1]->getAnimalAt(2) == card->getAnimalAt(0) ? 1 : 0;
 		paired[1] |= around[1]->getAnimalAt(3) == card->getAnimalAt(1) ? 1 : 0;
+	}
 		if (card->getAnimalAt(0) == card->getAnimalAt(1)) {
 			paired[1] |= paired[0];
-			paired[0] = 0; //on garde le compte pour seulement une carte
+			paired[0] = 0; 
 		}
-	}
 	/*Right*/
 	if (around[2] != 0) {
 		paired[1] |= around[2]->getAnimalAt(0) == card->getAnimalAt(1) ? 1 : 0;
 		paired[3] |= around[2]->getAnimalAt(2) == card->getAnimalAt(3) ? 1 : 0;
+	}
 		if (card->getAnimalAt(1) == card->getAnimalAt(3)) {
 			paired[3] |= paired[1];
-			paired[1] = 0; //on garde le compte pour seulement une carte
+			paired[1] = 0; 
 		}
-	}
 	/*Bottom*/
 	if (around[3] != 0) {
 		paired[3] |= around[3]->getAnimalAt(1) == card->getAnimalAt(3) ? 1 : 0;
 		paired[2] |= around[3]->getAnimalAt(0) == card->getAnimalAt(2) ? 1 : 0;
+	}
 		if (card->getAnimalAt(3) == card->getAnimalAt(2)) {
 			paired[2] |= paired[3];
-			paired[3] = 0; //on garde le compte pour seulement une carte
+			paired[3] = 0; 
 		}
-	}
 
 	return paired[0] + paired[1] + paired[2] + paired[3];
 }
